a644be7952eddd2abbcfc5958299db1977cc7be3
Clifford Mapesa <97587370+droffilc1@users.noreply.github.com>
2025-02-14 04:40:25 +0300

    Test service (#39)
    * test ci/cd
    
    * test: service

--- api/routes/books.py
+++ api/routes/books.py
@@ -33,12 +33,6 @@ db.books = {
 }
 
 
-
-@router.get("/test", status_code=status.HTTP_200_OK)
-async def test_deploy():
-    return {"status": "OK"}
-
-
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     db.add_book(book)
